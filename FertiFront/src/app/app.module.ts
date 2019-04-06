4ca8488d1534ff9e17e3182762f767b28664fcc4
Cristian Londono <cristian.londono@ceiba.com.co>
2019-04-06 00:22:07 -0500

    Se realiza CRUD de fuente

--- FertiFront/src/app/app.module.ts
+++ FertiFront/src/app/app.module.ts
@@ -1,9 +1,10 @@
+import { FormFuenteComponent } from './form-fuente/form-fuente.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
 import { registerLocaleData } from '@angular/common';
@@ -25,6 +26,7 @@ import { TableFuenteComponent } from './table-fuente/table-fuente.component';
 import { FormTipoFuenteEdicionComponent } from './form-tipo-fuente-edicion/form-tipo-fuente-edicion.component';
 import { FormRequerimientosCultivoEdicionComponent } from './form-requerimientos-cultivo-edicion/form-requerimientos-cultivo-edicion.component';
 import { FormRolEdicionComponent } from './form-rol-edicion/form-rol-edicion.component';
+import { FormFuenteEdicionComponent } from './form-fuente-edicion/form-fuente-edicion.component';
 
 registerLocaleData(en);
 
@@ -44,7 +46,9 @@ registerLocaleData(en);
     TableFuenteComponent,
     FormTipoFuenteEdicionComponent,
     FormRequerimientosCultivoEdicionComponent,
-    FormRolEdicionComponent
+    FormRolEdicionComponent,
+    FormFuenteEdicionComponent,
+    FormFuenteComponent
   ],
   imports: [
     BrowserModule,
@@ -57,17 +61,20 @@ registerLocaleData(en);
     AppRoutingModule
   ],
   exports: [
-    
+
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }, 
-    { provide: NZ_NOTIFICATION_CONFIG, useValue: { 
-      nzTop         : '24px',
-      nzBottom      : '24px',
-      nzPlacement   : 'topRight',
-      nzDuration    : 9000,
-      nzMaxStack    : 5,
+  providers: [{ provide: NZ_I18N, useValue: en_US },
+  {
+    provide: NZ_NOTIFICATION_CONFIG, useValue: {
+      nzTop: '24px',
+      nzBottom: '24px',
+      nzPlacement: 'topRight',
+      nzDuration: 9000,
+      nzMaxStack: 5,
       nzPauseOnHover: true,
-      nzAnimate     : true }}],
+      nzAnimate: true
+    }
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- FertiFront/src/app/form-fuente/form-fuente.component.ts
+++ FertiFront/src/app/form-fuente/form-fuente.component.ts
@@ -1,15 +1,64 @@
+import { Fuente } from './../clases_dominio/fuente';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TipoFuente } from '../clases_dominio/tipo-fuente';
+import { FuenteService } from '../servicios/fuente.service';
+import { TipoFuenteService } from '../servicios/tipo-fuente.service';
 
 @Component({
   selector: 'app-form-fuente',
   templateUrl: './form-fuente.component.html',
   styleUrls: ['./form-fuente.component.css']
 })
-export class FormFuenteComponent implements OnInit {
+export class FormFuenteComponent {
 
-  constructor() { }
+  formularioAgregarFuente: FormGroup;
+  public tipoFuentes: TipoFuente[];
+  enviado = false;
+  visible = false;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder, private servicioFuente: FuenteService,
+    private tipoFuenteServicio: TipoFuenteService) {
+  }
+
+  private crearFormulario(): void {
+    this.formularioAgregarFuente = this.fb.group({
+      tipoFuente: [null, [Validators.required]],
+      producto: [null, [Validators.required]],
+      estado: [false, [Validators.required]],
+    });
+  }
+
+  submit() {
+    this.enviado = true;
+    if (this.formularioAgregarFuente.invalid) {
+      return null;
+    }
+    const fuente = new Fuente();
+    fuente.tipoFuente = this.f.tipoFuente.value;
+    fuente.producto = this.f.producto.value;
+    fuente.estado = this.f.estado.value;
+
+    this.servicioFuente.updateOrCreate(fuente).subscribe(accion => {
+      this.servicioFuente.cargarDatos();
+      this.close();
+    });
+  }
+
+  get f() { return this.formularioAgregarFuente.controls; }
+
+  open(): void {
+    this.crearFormulario();
+    this.tipoFuenteServicio.getBackTipoFuente().subscribe(result => {
+      this.tipoFuentes = result;
+    });
+    this.visible = true;
+    this.enviado = false;
+  }
+
+  close(): void {
+    this.formularioAgregarFuente.reset();
+    this.visible = false;
   }
 
 }
