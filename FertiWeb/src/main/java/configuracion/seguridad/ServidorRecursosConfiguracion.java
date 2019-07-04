3738d8efc9b96a24928bd1b8ec0eb9f39b816cf0
CEIBA\juan.sanchez <juan.sanchez@ceiba.com.co>
2019-07-04 10:58:23 -0500

    Ajuste en configuracion

--- FertiWeb/src/main/java/configuracion/seguridad/ServidorRecursosConfiguracion.java
+++ FertiWeb/src/main/java/configuracion/seguridad/ServidorRecursosConfiguracion.java
@@ -22,16 +22,9 @@ public class ServidorRecursosConfiguracion extends ResourceServerConfigurerAdapt
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers(HttpMethod.POST, "/servicio_usuario/guardadoUsuario").permitAll()
-				.antMatchers(HttpMethod.GET, "/servicio_rol/consultaRoles").permitAll()
-				.antMatchers(HttpMethod.POST, "/servicio_recomendacion/generarRecomendacion").permitAll().anyRequest().authenticated()
+				.antMatchers(HttpMethod.GET, "/servicio_rol/consultaRoles").permitAll().anyRequest().authenticated()
 				.and().cors().configurationSource(corsConfigurationSource());
-
-
-		 /*
-		 * // http.headers().frameOptions().disable();
-		 * http.authorizeRequests().antMatchers("/h2-console/*").permitAll().anyRequest(
-		 * ).anonymous().and().cors() .configurationSource(corsConfigurationSource());
-		 */
+		
 	}
 
 	@Bean
